8a8cba9b3462014dcc4c6412f23f429bb71867ff
kang-mincheol <aaa6315@gmail.com>
2022-03-22 01:32:22 +0900

    마이레코드 랭킹페이지 퍼블리싱 PC

---
+++ record/landing/index.js
@@ -0,0 +1,13 @@
+function init() {
+    
+}
+
+function rankingMenuRemote(name) {
+    if(name != undefined) {
+        $("#record_ranking .ranking_menu_wrap .ranking_menu_btn").removeClass("on");
+        $("#record_ranking .ranking_menu_wrap .ranking_menu_btn[name="+name+"]").addClass("on");
+
+        $("#record_ranking .ranking_contents_box").removeClass("on");
+        $("#record_ranking .ranking_contents_box[name="+name+"]").addClass("on");
+    }
+}
--- record/landing/index.php
+++ record/landing/index.php
@@ -12,83 +12,333 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/record/record_sub_menu/record_sub_menu.
 ?>
 
 <div id="record_ranking">
-    <div class="section_title">TOP10</div>
-
-    <div class="ranking_header">
-        <div class="header_box rank">순위</div>
-        <div class="header_box total">3대</div>
-        <div class="header_box squat">Squat</div>
-        <div class="header_box benchpress">BenchPress</div>
-        <div class="header_box deadlift">DeadLift</div>
-        <div class="header_box name">닉네임</div>
+    <div class="ranking_menu_wrap">
+        <button class="ranking_menu_btn on" name="total" onclick="rankingMenuRemote('total');">종합</button>
+        <button class="ranking_menu_btn" name="squat" onclick="rankingMenuRemote('squat');">Squat</button>
+        <button class="ranking_menu_btn" name="bench" onclick="rankingMenuRemote('bench');">BenchPress</button>
+        <button class="ranking_menu_btn" name="dead" onclick="rankingMenuRemote('dead');">DeadLift</button>
     </div>
 
-    <div class="ranking_body">
-        <div class="body_row top">
-            <div class="body_box rank"><p class="number">1</p></div>
-            <div class="body_box total">440</div>
-            <div class="body_box squat">180</div>
-            <div class="body_box benchpress">100</div>
-            <div class="body_box deadlift">160</div>
-            <div class="body_box name">관리자</div>
+    <div class="ranking_contents_wrap">
+        <div class="ranking_contents_box on" name="total">
+            <div class="ranking_contents_header">
+                <div class="header_box rank">순위</div>
+                <div class="header_box total">3대</div>
+                <div class="header_box squat">Squat</div>
+                <div class="header_box benchpress">BenchPress</div>
+                <div class="header_box deadlift">DeadLift</div>
+                <div class="header_box name">닉네임</div>
+            </div>
+            <div class="ranking_contents_body">
+                <div class="body_row top">
+                <div class="body_box rank"><p class="number">1</p></div>
+                <div class="body_box total">440</div>
+                <div class="body_box squat">180</div>
+                <div class="body_box benchpress">100</div>
+                <div class="body_box deadlift">160</div>
+                <div class="body_box name">관리자</div>
+            </div>
+            <div class="body_row top">
+                <div class="body_box rank"><p class="number">2</p></div>
+                <div class="body_box total">440</div>
+                <div class="body_box squat">180</div>
+                <div class="body_box benchpress">100</div>
+                <div class="body_box deadlift">160</div>
+                <div class="body_box name">관리자</div>
+            </div>
+            <div class="body_row top">
+                <div class="body_box rank"><p class="number">3</p></div>
+                <div class="body_box total">440</div>
+                <div class="body_box squat">180</div>
+                <div class="body_box benchpress">100</div>
+                <div class="body_box deadlift">160</div>
+                <div class="body_box name">관리자</div>
+            </div>
+            <div class="body_row">
+                <div class="body_box rank">1</div>
+                <div class="body_box total">440</div>
+                <div class="body_box squat">180</div>
+                <div class="body_box benchpress">100</div>
+                <div class="body_box deadlift">160</div>
+                <div class="body_box name">관리자</div>
+            </div>
+            <div class="body_row">
+                <div class="body_box rank">1</div>
+                <div class="body_box total">440</div>
+                <div class="body_box squat">180</div>
+                <div class="body_box benchpress">100</div>
+                <div class="body_box deadlift">160</div>
+                <div class="body_box name">관리자</div>
+            </div>
+            <div class="body_row">
+                <div class="body_box rank">1</div>
+                <div class="body_box total">440</div>
+                <div class="body_box squat">180</div>
+                <div class="body_box benchpress">100</div>
+                <div class="body_box deadlift">160</div>
+                <div class="body_box name">관리자</div>
+            </div>
+            <div class="body_row">
+                <div class="body_box rank">1</div>
+                <div class="body_box total">440</div>
+                <div class="body_box squat">180</div>
+                <div class="body_box benchpress">100</div>
+                <div class="body_box deadlift">160</div>
+                <div class="body_box name">관리자</div>
+            </div>
+            <div class="body_row">
+                <div class="body_box rank">1</div>
+                <div class="body_box total">440</div>
+                <div class="body_box squat">180</div>
+                <div class="body_box benchpress">100</div>
+                <div class="body_box deadlift">160</div>
+                <div class="body_box name">관리자</div>
+            </div>
+            </div>
         </div>
-        <div class="body_row top">
-            <div class="body_box rank"><p class="number">2</p></div>
-            <div class="body_box total">440</div>
-            <div class="body_box squat">180</div>
-            <div class="body_box benchpress">100</div>
-            <div class="body_box deadlift">160</div>
-            <div class="body_box name">관리자</div>
+        <!-- total -->
+
+
+        <div class="ranking_contents_box" name="squat">
+            <div class="ranking_contents_header">
+                <div class="header_box rank">순위</div>
+                <div class="header_box total">3대</div>
+                <div class="header_box squat">Squat</div>
+                <div class="header_box benchpress">BenchPress</div>
+                <div class="header_box deadlift">DeadLift</div>
+                <div class="header_box name">닉네임</div>
+            </div>
+            <div class="ranking_contents_body">
+                <div class="body_row top">
+                <div class="body_box rank"><p class="number">1</p></div>
+                <div class="body_box total">440</div>
+                <div class="body_box squat">180</div>
+                <div class="body_box benchpress">100</div>
+                <div class="body_box deadlift">160</div>
+                <div class="body_box name">관리자</div>
+            </div>
+            <div class="body_row top">
+                <div class="body_box rank"><p class="number">2</p></div>
+                <div class="body_box total">440</div>
+                <div class="body_box squat">180</div>
+                <div class="body_box benchpress">100</div>
+                <div class="body_box deadlift">160</div>
+                <div class="body_box name">관리자</div>
+            </div>
+            <div class="body_row top">
+                <div class="body_box rank"><p class="number">3</p></div>
+                <div class="body_box total">440</div>
+                <div class="body_box squat">180</div>
+                <div class="body_box benchpress">100</div>
+                <div class="body_box deadlift">160</div>
+                <div class="body_box name">관리자</div>
+            </div>
+            <div class="body_row">
+                <div class="body_box rank">1</div>
+                <div class="body_box total">440</div>
+                <div class="body_box squat">180</div>
+                <div class="body_box benchpress">100</div>
+                <div class="body_box deadlift">160</div>
+                <div class="body_box name">관리자</div>
+            </div>
+            <div class="body_row">
+                <div class="body_box rank">1</div>
+                <div class="body_box total">440</div>
+                <div class="body_box squat">180</div>
+                <div class="body_box benchpress">100</div>
+                <div class="body_box deadlift">160</div>
+                <div class="body_box name">관리자</div>
+            </div>
+            <div class="body_row">
+                <div class="body_box rank">1</div>
+                <div class="body_box total">440</div>
+                <div class="body_box squat">180</div>
+                <div class="body_box benchpress">100</div>
+                <div class="body_box deadlift">160</div>
+                <div class="body_box name">관리자</div>
+            </div>
+            <div class="body_row">
+                <div class="body_box rank">1</div>
+                <div class="body_box total">440</div>
+                <div class="body_box squat">180</div>
+                <div class="body_box benchpress">100</div>
+                <div class="body_box deadlift">160</div>
+                <div class="body_box name">관리자</div>
+            </div>
+            <div class="body_row">
+                <div class="body_box rank">1</div>
+                <div class="body_box total">440</div>
+                <div class="body_box squat">180</div>
+                <div class="body_box benchpress">100</div>
+                <div class="body_box deadlift">160</div>
+                <div class="body_box name">관리자</div>
+            </div>
+            </div>
         </div>
-        <div class="body_row top">
-            <div class="body_box rank"><p class="number">3</p></div>
-            <div class="body_box total">440</div>
-            <div class="body_box squat">180</div>
-            <div class="body_box benchpress">100</div>
-            <div class="body_box deadlift">160</div>
-            <div class="body_box name">관리자</div>
+        <!-- squat -->
+
+
+        <div class="ranking_contents_box" name="bench">
+            <div class="ranking_contents_header">
+                <div class="header_box rank">순위</div>
+                <div class="header_box total">3대</div>
+                <div class="header_box squat">Squat</div>
+                <div class="header_box benchpress">BenchPress</div>
+                <div class="header_box deadlift">DeadLift</div>
+                <div class="header_box name">닉네임</div>
+            </div>
+            <div class="ranking_contents_body">
+                <div class="body_row top">
+                <div class="body_box rank"><p class="number">1</p></div>
+                <div class="body_box total">440</div>
+                <div class="body_box squat">180</div>
+                <div class="body_box benchpress">100</div>
+                <div class="body_box deadlift">160</div>
+                <div class="body_box name">관리자</div>
+            </div>
+            <div class="body_row top">
+                <div class="body_box rank"><p class="number">2</p></div>
+                <div class="body_box total">440</div>
+                <div class="body_box squat">180</div>
+                <div class="body_box benchpress">100</div>
+                <div class="body_box deadlift">160</div>
+                <div class="body_box name">관리자</div>
+            </div>
+            <div class="body_row top">
+                <div class="body_box rank"><p class="number">3</p></div>
+                <div class="body_box total">440</div>
+                <div class="body_box squat">180</div>
+                <div class="body_box benchpress">100</div>
+                <div class="body_box deadlift">160</div>
+                <div class="body_box name">관리자</div>
+            </div>
+            <div class="body_row">
+                <div class="body_box rank">1</div>
+                <div class="body_box total">440</div>
+                <div class="body_box squat">180</div>
+                <div class="body_box benchpress">100</div>
+                <div class="body_box deadlift">160</div>
+                <div class="body_box name">관리자</div>
+            </div>
+            <div class="body_row">
+                <div class="body_box rank">1</div>
+                <div class="body_box total">440</div>
+                <div class="body_box squat">180</div>
+                <div class="body_box benchpress">100</div>
+                <div class="body_box deadlift">160</div>
+                <div class="body_box name">관리자</div>
+            </div>
+            <div class="body_row">
+                <div class="body_box rank">1</div>
+                <div class="body_box total">440</div>
+                <div class="body_box squat">180</div>
+                <div class="body_box benchpress">100</div>
+                <div class="body_box deadlift">160</div>
+                <div class="body_box name">관리자</div>
+            </div>
+            <div class="body_row">
+                <div class="body_box rank">1</div>
+                <div class="body_box total">440</div>
+                <div class="body_box squat">180</div>
+                <div class="body_box benchpress">100</div>
+                <div class="body_box deadlift">160</div>
+                <div class="body_box name">관리자</div>
+            </div>
+            <div class="body_row">
+                <div class="body_box rank">1</div>
+                <div class="body_box total">440</div>
+                <div class="body_box squat">180</div>
+                <div class="body_box benchpress">100</div>
+                <div class="body_box deadlift">160</div>
+                <div class="body_box name">관리자</div>
+            </div>
+            </div>
         </div>
-        <div class="body_row">
-            <div class="body_box rank">1</div>
-            <div class="body_box total">440</div>
-            <div class="body_box squat">180</div>
-            <div class="body_box benchpress">100</div>
-            <div class="body_box deadlift">160</div>
-            <div class="body_box name">관리자</div>
+        <!-- bench -->
+
+
+        <div class="ranking_contents_box" name="dead">
+            <div class="ranking_contents_header">
+                <div class="header_box rank">순위</div>
+                <div class="header_box total">3대</div>
+                <div class="header_box squat">Squat</div>
+                <div class="header_box benchpress">BenchPress</div>
+                <div class="header_box deadlift">DeadLift</div>
+                <div class="header_box name">닉네임</div>
+            </div>
+            <div class="ranking_contents_body">
+                <div class="body_row top">
+                <div class="body_box rank"><p class="number">1</p></div>
+                <div class="body_box total">440</div>
+                <div class="body_box squat">180</div>
+                <div class="body_box benchpress">100</div>
+                <div class="body_box deadlift">160</div>
+                <div class="body_box name">관리자</div>
+            </div>
+            <div class="body_row top">
+                <div class="body_box rank"><p class="number">2</p></div>
+                <div class="body_box total">440</div>
+                <div class="body_box squat">180</div>
+                <div class="body_box benchpress">100</div>
+                <div class="body_box deadlift">160</div>
+                <div class="body_box name">관리자</div>
+            </div>
+            <div class="body_row top">
+                <div class="body_box rank"><p class="number">3</p></div>
+                <div class="body_box total">440</div>
+                <div class="body_box squat">180</div>
+                <div class="body_box benchpress">100</div>
+                <div class="body_box deadlift">160</div>
+                <div class="body_box name">관리자</div>
+            </div>
+            <div class="body_row">
+                <div class="body_box rank">1</div>
+                <div class="body_box total">440</div>
+                <div class="body_box squat">180</div>
+                <div class="body_box benchpress">100</div>
+                <div class="body_box deadlift">160</div>
+                <div class="body_box name">관리자</div>
+            </div>
+            <div class="body_row">
+                <div class="body_box rank">1</div>
+                <div class="body_box total">440</div>
+                <div class="body_box squat">180</div>
+                <div class="body_box benchpress">100</div>
+                <div class="body_box deadlift">160</div>
+                <div class="body_box name">관리자</div>
+            </div>
+            <div class="body_row">
+                <div class="body_box rank">1</div>
+                <div class="body_box total">440</div>
+                <div class="body_box squat">180</div>
+                <div class="body_box benchpress">100</div>
+                <div class="body_box deadlift">160</div>
+                <div class="body_box name">관리자</div>
+            </div>
+            <div class="body_row">
+                <div class="body_box rank">1</div>
+                <div class="body_box total">440</div>
+                <div class="body_box squat">180</div>
+                <div class="body_box benchpress">100</div>
+                <div class="body_box deadlift">160</div>
+                <div class="body_box name">관리자</div>
+            </div>
+            <div class="body_row">
+                <div class="body_box rank">1</div>
+                <div class="body_box total">440</div>
+                <div class="body_box squat">180</div>
+                <div class="body_box benchpress">100</div>
+                <div class="body_box deadlift">160</div>
+                <div class="body_box name">관리자</div>
+            </div>
+            </div>
         </div>
-        <div class="body_row">
-            <div class="body_box rank">1</div>
-            <div class="body_box total">440</div>
-            <div class="body_box squat">180</div>
-            <div class="body_box benchpress">100</div>
-            <div class="body_box deadlift">160</div>
-            <div class="body_box name">관리자</div>
-        </div>
-        <div class="body_row">
-            <div class="body_box rank">1</div>
-            <div class="body_box total">440</div>
-            <div class="body_box squat">180</div>
-            <div class="body_box benchpress">100</div>
-            <div class="body_box deadlift">160</div>
-            <div class="body_box name">관리자</div>
-        </div>
-        <div class="body_row">
-            <div class="body_box rank">1</div>
-            <div class="body_box total">440</div>
-            <div class="body_box squat">180</div>
-            <div class="body_box benchpress">100</div>
-            <div class="body_box deadlift">160</div>
-            <div class="body_box name">관리자</div>
-        </div>
-        <div class="body_row">
-            <div class="body_box rank">1</div>
-            <div class="body_box total">440</div>
-            <div class="body_box squat">180</div>
-            <div class="body_box benchpress">100</div>
-            <div class="body_box deadlift">160</div>
-            <div class="body_box name">관리자</div>
-        </div>
+        <!-- dead -->
     </div>
+    <!-- ranking_contents_wrap -->
+
+
+
 </div>
 <!-- record_ranking -->
 
