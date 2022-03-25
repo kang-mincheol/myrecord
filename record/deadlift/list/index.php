b630616820faf479a640395550ceb1d90cb481d4
kang-mincheol <aaa6315@gmail.com>
2022-03-26 04:31:43 +0900

    마이레코드 서브메뉴 추가

--- record/deadlift/list/index.php
+++ record/deadlift/list/index.php
@@ -8,6 +8,9 @@ echo css_load('/record/deadlift/list/index.css');
 
 <div id="page_title">마이레코드 ＞ DeadLift</div>
 
+<?
+include_once($_SERVER['DOCUMENT_ROOT'].'/record/record_sub_menu/record_sub_menu.php');
+?>
 
 <div id="board_wrap">
 
