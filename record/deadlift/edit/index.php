14296e95a51bc0706dc44f89e147db6b0a4df477
kang-mincheol <aaa6315@gmail.com>
2022-03-27 03:43:59 +0900

    데드리프트 에딧페이지 파일추가

--- record/deadlift/edit/index.php
+++ record/deadlift/edit/index.php
@@ -8,7 +8,9 @@ echo css_load('/record/deadlift/edit/index.css');
 
 <div id="page_title">마이레코드 ＞ DeadLift</div>
 
-
+<?
+include_once($_SERVER['DOCUMENT_ROOT'].'/component/sub_menu/record_sub_menu/record_sub_menu.php');
+?>
 
 
 
