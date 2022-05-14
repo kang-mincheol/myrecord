66c36fa7c35f0304af2cba7f3eee56d3195ad74e
kang-mincheol <aaa6315@gmail.com>
2022-05-15 01:57:58 +0900

    내기록 모바일 css 작업

--- record/my_record/index.js
+++ record/my_record/index.js
@@ -20,7 +20,7 @@ function getMyRecordData() {
                     if(thisData["record_id"]) {
                         renderHtml +=
                             '<div class="record_weight">'+
-                                '<a class="record_link" href="/record/record_regist/?record_id='+thisData["record_id"]+'">'+thisData["weight"]+'</a>'+
+                                '<a class="record_link" href="/record/record_regist/?record_id='+thisData["record_id"]+'">'+thisData["weight"]+'<i class="fa-solid fa-arrow-up-right-from-square"></i></a>'+
                             '</div>'+
                             '<div class="record_status">'+
                                 '<i class="fa-solid fa-circle-check '+thisData["status_color"]+'"></i>'+
