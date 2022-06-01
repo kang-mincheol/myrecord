c67d3e1149137c8a78e2f8e807976a39b7acb775
kang-mincheol <aaa6315@gmail.com>
2022-06-01 13:24:00 +0900

    3대 종합순위 api 작업

---
+++ api/record/landing/get.record_total_ranking.php
@@ -0,0 +1,82 @@
+<?php
+include_once($_SERVER["DOCUMENT_ROOT"]."/api/common.php");
+
+header("Content-Type: application/json; charset=UTF-8");
+
+$returnArray = array(
+    "code"=>"SUCCESS",
+    "msg"=>"정상 처리되었습니다"
+);
+
+
+$ranking_sql = "
+    Select	Account.id, Account.user_id, Account.user_nickname,
+		IfNull(Squat.weight, 0) as squat,
+        IfNull(Squat.id, '-') as squat_id,
+        IfNull(Benchpress.weight, 0) as bench,
+        IfNull(Benchpress.id, '-') as bench_id,
+        IfNull(Deadlift.weight, 0) as dead,
+        IfNull(Deadlift.id, '-') as dead_id,
+        Sum(
+			IfNull(Squat.weight, 0) +
+			IfNull(Benchpress.weight, 0) +
+			IfNull(Deadlift.weight, 0)
+		) as total_sum
+    From	Account
+    Left Outer Join (
+        Select	max(record_weight) as weight, account_id, id
+        From	tb_record_request
+        Where	record_type = 1
+        And		status = 2
+        Group by account_id, id
+    ) Squat
+    On	Account.id = Squat.account_id
+    Left Outer Join (
+        Select	max(record_weight) as weight, account_id, id
+        From	tb_record_request
+        Where	record_type = 2
+        And		status = 2
+        Group by account_id, id
+    ) Benchpress
+    On	Account.id = Benchpress.account_id
+    Left Outer Join (
+        Select	max(record_weight) as weight, account_id, id
+        From	tb_record_request
+        Where	record_type = 3
+        And		status = 2
+        Group by account_id, id
+    ) Deadlift
+    On	Account.id = Deadlift.account_id
+
+    Where	(Squat.weight > 0 Or Benchpress.weight > 0 Or Deadlift.weight)
+
+    Group by Account.id, Account.user_id, Account.user_nickname, squat, bench, dead, squat_id, bench_id, dead_id
+
+    Order by total_sum Desc
+    Limit 0, 10
+";
+$total_ranking = $PDO -> fetchAll($ranking_sql);
+
+foreach($total_ranking as $key => $value) {
+    $returnArray["data"][] = array(
+        "3대" => $value["total_sum"],
+        "squat" => $value["squat"],
+        "sqaut_record_id" => $value["squat_id"],
+        "benchpress" => $value["bench"],
+        "benchpress_record_id" => $value["bench_id"],
+        "deadlift" => $value["deadlift"],
+        "deadlift_record_id" => $value["dead_id"],
+        "nickname" => $value["user_nickname"]
+    );
+}
+
+
+
+
+
+
+
+
+
+echo json_encode($returnArray, JSON_UNESCAPED_UNICODE); exit;
+?>
