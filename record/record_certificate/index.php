63a698c1da0e20844b259ec199acb08bb1330b7b
kang-mincheol <aaa6315@gmail.com>
2022-09-16 02:03:04 +0900

    마이레코드 기록 인증서 페이지 파일 생성

---
+++ record/record_certificate/index.php
@@ -0,0 +1,40 @@
+<?
+include_once($_SERVER['DOCUMENT_ROOT'].'/common.php');   // 기본파일 로드
+include_once($_SERVER['DOCUMENT_ROOT'].'/header.php');   // 헤더파일 로드
+
+
+if(!$is_member) {
+    echo '<script>myrecordAlert(\'on\', \'로그인 후 이용해주세요\', \'알림\', \'location.href="/account/login/"\');</script>';
+    exit;
+}
+?>
+
+
+<?
+echo css_load('/record/record_certificate/index.css');
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?
+echo script_load('/record/record_certificate/index.js');
+?>
+<script>
+$(function () {
+    init();
+});
+</script>
+<?
+include_once($_SERVER['DOCUMENT_ROOT'].'/footer.php');   // 푸터파일 로드
+?>
