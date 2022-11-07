f37dee079fd1546e2f89e62fe60231c7eb6b3fb9
kang-mincheol <aaa6315@gmail.com>
2022-11-08 01:20:04 +0900

    404 페이지 수정

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?
+include_once('common.php');   // 기본파일 로드
+include_once('header.php');   // 헤더파일 로드
+?>
+<?
+echo css_load('/common/404.css');
+?>
+
+
+<div id="section_1">
+    <p class="text_0">ERROR</p>
+    <p class="text_1">404 Not Found ❌</p>
+    <p class="text_2">
+        페이지를 찾을 수 없습니다. ❌
+    </p>
+</div>
