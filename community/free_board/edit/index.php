9ceff11b3b90af86095464819b8a0df16fd048b5
kang-mincheol <aaa6315@gmail.com>
2023-01-30 01:38:12 +0900

    로그인 유도처리

--- community/free_board/edit/index.php
+++ community/free_board/edit/index.php
@@ -2,6 +2,10 @@
 include_once($_SERVER['DOCUMENT_ROOT'].'/common.php');   // 기본파일 로드
 include_once($_SERVER['DOCUMENT_ROOT'].'/header.php');   // 헤더파일 로드
 
+if(!$is_member) {
+    echo "<script>myrecordAlert('on', '로그인 후 이용해주세요.', '알림', 'location.href=\'/account/login\'');</script>"; exit;
+}
+
 echo css_load('/community/free_board/edit/index.css');
 ?>
 
