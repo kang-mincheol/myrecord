42ee7e00e6be08776e5f5bba270b73b10cf3449c
mincheol-kang <aaa6315@gmail.com>
2024-12-06 13:04:04 +0900

    feature/list param 정보 세팅

--- community/free_board/list/index.js
+++ community/free_board/list/index.js
@@ -1,7 +1,37 @@
 const pageInit = () => {
-  renderFreeBoardList();
+  getFreeBoardList();
 };
 
-const getFreeBoardList = async () => {};
+const getFreeBoardList = async () => {
+  const param = {
+    pageIndex: listInfo.pageIndex,
+    pageRow: listInfo.pageRow,
+  };
 
-const renderFreeBoardList = (list) => {};
+  const searchKeyword = $("#search_keyword").val();
+
+  if (searchKeyword !== "") {
+    param["searchKey"] = $("#search_key").val();
+    param["searchValue"] = searchKeyword;
+  }
+
+  $.ajax({
+    type: "POST",
+    data: JSON.stringify(param),
+    url: "/api/free_board/get.free_board_list_data.php",
+    success: (data) => {
+      loadingOff();
+      if (data["code"] === "SUCCESS") {
+        renderFreeBoardList(data["data"]);
+      }
+    },
+    error: (error) => {
+      loadingOff();
+      console.log(error);
+    },
+  });
+};
+
+const renderFreeBoardList = (list) => {
+  console.log("list => ", list);
+};
