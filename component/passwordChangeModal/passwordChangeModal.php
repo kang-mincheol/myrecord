6d3a91df6a0d9f8ff034e3b2f81ac5ccae6aa578
mincheol-kang <aaa6315@gmail.com>
2024-12-12 13:15:02 +0900

    feature/비밀번호 모달 테스트 코드 제거

--- component/passwordChangeModal/passwordChangeModal.php
+++ component/passwordChangeModal/passwordChangeModal.php
@@ -4,7 +4,7 @@ if (!defined('NO_ALONE')) exit; // 개별 페이지 접근 불가
 echo css_load('/component/passwordChangeModal/passwordChangeModal.css');
 ?>
 
-<div id="passwordChangeModal" class="on">
+<div id="passwordChangeModal" class="">
   <div class="modal-wrapper">
     <div class="modal-header">비밀번호 변경</div>
     <div class="modal-body">
