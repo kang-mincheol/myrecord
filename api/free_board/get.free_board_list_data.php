e4fd7816358cb4fbed33b26cfe98ee8ec4ae1ae1
mincheol <aaa6315@gmail.com>
2024-12-25 23:56:37 +0900

    feature/test

--- api/free_board/get.free_board_list_data.php
+++ api/free_board/get.free_board_list_data.php
@@ -32,7 +32,7 @@ foreach($freeBoardListData as $key => $value) {
         "title" => $value["title"],
         "nickname" => $value["user_nickname"],
         "view_count" => $value["view_count"],
-        "create_date" => date("Y.m.d", strtotime($value["create_date"])),
+        "test" => date("Y.m.d", strtotime($value["create_date"])),
     );
 }
 
