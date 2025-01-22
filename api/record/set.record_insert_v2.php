37781dfbb46f3ec9ab433ecd00140a81716b015a
mincheol-kang <aaa6315@gmail.com>
2025-01-22 12:51:38 +0900

    feature/레코드 등록 api 작업 중

--- api/record/set.record_insert_v2.php
+++ api/record/set.record_insert_v2.php
@@ -19,10 +19,18 @@ $record_type = $data['record_type'];
 
 $record_type = preg_replace('/[^0-9]+/u', '', $record_type);
 
-if($record_type == '') {
+if($record_type === "") {
     $returnArray["code"] = "PARAM_ERROR";
     $returnArray["msg"] = "등록할 기록을 선택해주세요";
     echo json_encode($returnArray, JSON_UNESCAPED_UNICODE); exit;
 }
 
+$has_record_type = Record::hasRecordType($record_type);
+if ($has_record_type === false) {
+    $returnArray["code"] = "RECORD_TYPE_ERROR";
+    $returnArray["msg"] = "존재하지 않는 종목입니다.";
+    echo json_encode($returnArray, JSON_UNESCAPED_UNICODE); exit;
+}
+
+echo json_encode($returnArray, JSON_UNESCAPED_UNICODE); exit;
 ?>
