65502c35e29b9566263d5b88afaabe1fdf0db144
mincheol <aaa6315@gmail.com>
2026-03-15 20:09:07 +0900

    feat: 기록 리스트 페이지 수정

--- record/my_record/index.php
+++ record/my_record/index.php
@@ -2,6 +2,7 @@
 include_once($_SERVER['DOCUMENT_ROOT'].'/common.php');   // 기본파일 로드
 include_once($_SERVER['DOCUMENT_ROOT'].'/header.php');   // 헤더파일 로드
 
+echo css_load('/common/record_pages.css');
 echo css_load('/record/my_record/index.css');
 
 if(!$is_member) {
@@ -11,7 +12,12 @@ if(!$is_member) {
 ?>
 
 
-<div id="page_title">마이레코드 ＞ 내 기록</div>
+<div class="record_page_header">
+    <div class="page_header_inner">
+        <p class="page_title_text">내 기록</p>
+        <p class="page_sub_text">나의 종목별 최고 기록을 확인해 보세요</p>
+    </div>
+</div>
 
 <?php
 include_once($_SERVER['DOCUMENT_ROOT'].'/component/sub_menu/record_sub_menu/record_sub_menu.php');
@@ -19,36 +25,9 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/component/sub_menu/record_sub_menu/reco
 
 
 <div id="my_record_wrap">
-
-    <div class="record_wrap">
-
-<!--
-        <div class="record_box">
-            <div class="box_title">DeadLift</div>
-            <div class="record_info_box">
-                <p class="empty_line">기록 없음</p>
-                <div class="record_weight">100KG</div>
-                <div class="record_status">
-                    <i class="fa-solid fa-circle-check blue"></i>
-                    <p class="status_text">심사중</p>
-                </div>
-            </div>
-        </div>
--->
-
-    </div>
-
-
-
-
-
-
+    <div class="record_wrap"></div>
 </div>
 <!-- my_record_wrap -->
-
-
-
-
 
 
 <?php
