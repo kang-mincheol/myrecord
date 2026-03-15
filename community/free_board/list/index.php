932d0fcc0918fd098ead32ef34e40559971cf330
mincheol <aaa6315@gmail.com>
2026-03-15 21:08:54 +0900

    feat: 자유게시판 디자인 수정

--- community/free_board/list/index.php
+++ community/free_board/list/index.php
@@ -14,56 +14,39 @@ echo css_load('/community/free_board/list/index.css');
 </div>
 
 
-<div id="board_wrap">
-    <div class="board_container">
+<div class="free_board_wrap">
 
-        <div class="board_container_head">
-            <div class="board_header_wrap">
-                <div class="top_box">
-                    <div class="header_box title">제목</div>
-                </div>
-                <div class="bottom_box">
-                    <div class="header_box writer">작성자</div>
-                    <div class="header_box view">조회수</div>
-                    <div class="header_box date">작성일</div>
-                </div>
+    <div id="board_wrap">
+        <div class="board_container">
+            <div class="board_body_wrap">
+                <!-- JS renders board_row items here -->
             </div>
         </div>
+    </div>
 
-        <div class="board_body_wrap">
-            <!-- JS renders board_row items here -->
+    <div id="pagingWrap" class="paging_wrap"></div>
+
+    <div class="board_footer_bar">
+        <a class="write_btn" href="/community/free_board/edit/">
+            <i class="fa-solid fa-pen-to-square"></i> 글쓰기
+        </a>
+        <div class="search_area">
+            <select id="search_key" class="search_select">
+                <option value="title">제목</option>
+                <option value="contents">내용</option>
+                <option value="writer">작성자</option>
+            </select>
+            <div class="search_input_wrap">
+                <input id="search_keyword" type="text" placeholder="검색어를 입력하세요"/>
+                <button class="search_btn" onclick="listSearch();" title="검색">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </button>
+            </div>
         </div>
-
     </div>
-    <!-- board_container -->
+
 </div>
-<!-- board_wrap -->
-
-
-<div id="pagingWrap" class="paging_wrap"></div>
-
-
-<div class="write_btn_wrap">
-    <a class="write_btn" href="/community/free_board/edit/">
-        <i class="fa-solid fa-pen-to-square"></i> 글쓰기
-    </a>
-</div>
-
-
-<div class="board_search_wrap">
-    <select id="search_key" class="search_select">
-        <option value="title">제목</option>
-        <option value="contents">내용</option>
-        <option value="writer">작성자</option>
-    </select>
-    <div class="search_keyword_wrap">
-        <input id="search_keyword" type="text" placeholder="검색어를 입력하세요"/>
-        <button class="search_btn" onclick="listSearch();" title="검색">
-            <i class="fa-solid fa-magnifying-glass"></i>
-        </button>
-    </div>
-</div>
-<!-- board_search_wrap -->
+<!-- free_board_wrap -->
 
 
 <?php
