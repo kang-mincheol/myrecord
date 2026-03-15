12393204fbedcdf10e8b58193fd2ec70fc132e57
mincheol <aaa6315@gmail.com>
2026-03-15 21:51:17 +0900

    feat: 약관 페이지 업데이트

---
+++ policy/marketing/index.php
@@ -0,0 +1,58 @@
+<?php
+include_once($_SERVER['DOCUMENT_ROOT'].'/common.php');
+include_once($_SERVER['DOCUMENT_ROOT'].'/header.php');
+
+echo css_load('/policy/policy.css');
+?>
+
+<div class="policy_page_header">
+    <div class="page_header_inner">
+        <p class="page_title_text">마케팅 수신 동의</p>
+        <p class="page_updated_text">시행일자 : 2022년 09월 06일</p>
+    </div>
+</div>
+
+<div class="policy_wrap">
+    <div class="policy_card">
+
+        <p class="policy_intro">
+            마이레코드는 이용자에게 더 나은 서비스와 혜택 정보를 제공하기 위해 아래와 같이 마케팅 정보 수신에 대한 동의를 구합니다.
+        </p>
+
+        <div class="policy_article">
+            <p class="article_title">1. 광고성 정보의 이용목적</p>
+            <div class="article_body">
+                <p>마이레코드가 제공하는 이용자 맞춤형 서비스 및 상품 추천, 각종 경품 행사, 이벤트 등의 광고성 정보를 전자우편, 문자(SMS 또는 카카오 알림톡), 푸시, 전화 등을 통해 이용자에게 제공합니다.</p>
+                <ul>
+                    <li>마케팅 수신 동의는 거부하실 수 있으며 동의 이후에라도 고객의 의사에 따라 동의를 철회할 수 있습니다.</li>
+                    <li>동의를 거부하시더라도 마이레코드가 제공하는 기본 서비스의 이용에는 제한이 없습니다.</li>
+                    <li>단, 할인, 이벤트 및 이용자 맞춤형 상품 추천 등의 마케팅 정보 안내 서비스가 제한됩니다.</li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="policy_article">
+            <p class="article_title">2. 미동의 시 불이익 사항</p>
+            <div class="article_body">
+                <p>개인정보보호법 제22조 제5항에 의해 선택정보 사항에 대해서는 동의를 거부하시더라도 서비스 이용에 제한이 없습니다. 단, 할인, 이벤트 및 이용자 맞춤형 상품 추천 등의 마케팅 정보 안내 서비스가 제한됩니다.</p>
+            </div>
+        </div>
+
+        <div class="policy_article">
+            <p class="article_title">3. 서비스 정보 수신 동의 철회</p>
+            <div class="article_body">
+                <p>마이레코드에서 제공하는 마케팅 정보를 원하지 않을 경우 '내 정보' 페이지에서 철회를 요청할 수 있습니다. 또한 향후 마케팅 활용에 새롭게 동의하고자 하는 경우에도 '내 정보' 페이지에서 동의하실 수 있습니다.</p>
+            </div>
+        </div>
+
+        <div class="policy_footer_note">
+            <p>- 공고일자 : 2022년 09월 01일</p>
+            <p>- 시행일자 : 2022년 09월 06일</p>
+        </div>
+
+    </div>
+</div>
+
+<?php
+include_once($_SERVER['DOCUMENT_ROOT'].'/footer.php');
+?>
