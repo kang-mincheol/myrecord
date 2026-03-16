bcf8929dd75cffd856eb0904e0d7b189fd037e92
mincheol <aaa6315@gmail.com>
2026-03-17 01:35:18 +0900

    feat: 비밀번호 모달 디자인 수정

--- component/passwordChangeModal/passwordChangeModal.php
+++ component/passwordChangeModal/passwordChangeModal.php
@@ -4,40 +4,44 @@ if (!defined('NO_ALONE')) exit; // 개별 페이지 접근 불가
 echo css_load('/component/passwordChangeModal/passwordChangeModal.css');
 ?>
 
-<div id="passwordChangeModal" class="">
-  <div class="modal-wrapper">
-    <div class="modal-header">비밀번호 변경</div>
-    <div class="modal-body">
-      <div class="myrecord_input_wrap now-password-wrap">
-          <div class="label_box">
-              <label class="wrap_label" for="now_password">현재 비밀번호</label>
-          </div>
-          <div class="form_value_box">
-              <input id="now_password" class="input_text" onkeyup="inputOnkeyupEvent(this);" type="password" placeholder="현재 비밀번호를 입력해 주세요"/>
-          </div>
-      </div>
-      <div class="myrecord_input_wrap new-password-wrap">
-          <div class="label_box">
-              <label class="wrap_label" for="new_password">새 비밀번호</label>
-          </div>
-          <div class="form_value_box">
-              <input id="new_password" class="input_text" onkeyup="inputOnkeyupEvent(this);" type="password" placeholder="영문, 숫자, 특수문자 포함 8~15자리"/>
-          </div>
-      </div>
-      <div class="myrecord_input_wrap new-password-check-wrap last">
-          <div class="label_box">
-              <label class="wrap_label" for="new_password_check">새 비밀번호 확인</label>
-          </div>
-          <div class="form_value_box">
-              <input id="new_password_check" class="input_text" onkeyup="inputOnkeyupEvent(this);" type="password" placeholder="새 비밀번호를 재입력해 주세요"/>
-          </div>
-      </div>
+<div id="passwordChangeModal">
+    <div class="pcm_overlay"></div>
+    <div class="pcm_box">
+
+        <div class="pcm_head">
+            <p class="pcm_title"><i class="fa-solid fa-key"></i> 비밀번호 변경</p>
+            <button class="pcm_close_btn" onclick="PasswordChangeModal.handler(false);" title="닫기">
+                <i class="fa-solid fa-xmark"></i>
+            </button>
+        </div>
+
+        <div class="pcm_body">
+
+            <div class="pcm_field">
+                <label class="pcm_label" for="now_password">현재 비밀번호</label>
+                <input id="now_password" class="pcm_input" onkeyup="inputOnkeyupEvent(this);" type="password" placeholder="현재 비밀번호를 입력해 주세요" autocomplete="current-password"/>
+            </div>
+
+            <div class="pcm_field">
+                <label class="pcm_label" for="new_password">새 비밀번호</label>
+                <input id="new_password" class="pcm_input" onkeyup="inputOnkeyupEvent(this);" type="password" placeholder="영문, 숫자, 특수문자 포함 8~15자리" autocomplete="new-password"/>
+            </div>
+
+            <div class="pcm_field">
+                <label class="pcm_label" for="new_password_check">새 비밀번호 확인</label>
+                <input id="new_password_check" class="pcm_input" onkeyup="inputOnkeyupEvent(this);" type="password" placeholder="새 비밀번호를 재입력해 주세요" autocomplete="new-password"/>
+            </div>
+
+        </div>
+
+        <div class="pcm_foot">
+            <button class="pcm_btn pcm_btn_cancel" onclick="PasswordChangeModal.handler(false);">취소</button>
+            <button class="pcm_btn pcm_btn_submit" onclick="PasswordChangeModal.setPassword();">
+                <i class="fa-solid fa-check"></i> 변경하기
+            </button>
+        </div>
+
     </div>
-    <div class="modal-btn-wrapper">
-      <button class="modal-btn change" onclick="PasswordChangeModal.setPassword();">비밀번호 변경</button>
-      <button class="modal-btn close" onclick="PasswordChangeModal.handler(false);">닫기</button>
-    </div>
-  </div>
 </div>
 
 <?php
